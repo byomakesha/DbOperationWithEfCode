32958470f6b2ccd2402d88bbc395a46a80898752
sures.mishra@lns.maersk.com <sures.mishra@lns.maersk.com>
2026-05-28 12:08:40 +0530

    commit model files

---
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/CurrencyType.cs
@@ -0,0 +1,9 @@
+﻿namespace DbOperationsWithEFCoreApp.Data
+{
+    public class CurrencyType
+    {
+        public int Id { get; set; }
+        public int Currency { get; set; }
+        public int Description { get; set; }
+    }
+}
---
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Data/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace DbOperationsWithEFCoreApp.Data
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Description { get; set; }
+    }
+}
